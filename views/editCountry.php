af51c713633cef2fd05799d5f7e1f86fc44fbc5a
Jad-Bel <jadthegamer06@gmail.com>
2024-12-13 22:54:45 +0100

    Edit a country complet but needs some improvement

--- views/editCountry.php
+++ views/editCountry.php
@@ -1,47 +1,4 @@
 <?php
-/*include "/xampp/htdocs/africa-geo-junior/views/connect.php";
-
-$nom = "";
-$population = "";
-$langue = "";
-$continent = "";
-
-$succesMessage = "";
-$errorMessage = "";
-
-
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nom = $_POST["nom"];
-    $population = $_POST["population"];
-    $langue = $_POST["langue"];
-    $continent = $_POST["continent"];
-
-    do {
-        if (empty($nom) || empty($population) || empty($langue)) {
-            $errorMessage = "All the fields are required";
-            break;
-        }
-
-        // insert new country to the db
-        $sql = "INSERT INTO pays (`nom`, `population`, `langue`, `continent_id`) VALUES ('$nom', '$population', '$langue', '$continent')";
-        $result = $connect -> query($sql);
-
-        if (!$result) {
-            $errorMessage = "Invalid query " . $connect->error;
-            break;
-        }
-
-        $nom = "";
-        $population = "";
-        $langue = "";
-
-        $succesMessage = "Country added correctly";
-
-        header("location: /africa-geo-junior/adminPage.php");
-        exit;
-    } while (false);
-}*/
-
 include "/xampp/htdocs/africa-geo-junior/views/connect.php";
 
 $id = "";
@@ -54,61 +11,100 @@ $succesMessage = "";
 $errorMessage = "";
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-// GET method: show the data of the country;
+    // GET method: show the data of the country;
 
     if (!isset($_GET["id_pays"]) || empty($_GET["id_pays"])) {
         header("location: /africa-geo-junior/views/editCountry.php");
         exit;
     }
-        $id = intval($_GET["id_pays"]);
+    $id = intval($_GET["id_pays"]);
 
-        $sql = "SELECT * FROM pays WHERE id_pays = $id";
-        $result = $connect -> query($sql);
+    $sql = "SELECT * FROM pays WHERE id_pays = $id";
+    $result = $connect->query($sql);
 
-        $row = $result->fetch_assoc();
-        $nom = $row['nom'];
-        $population = $row['Population'];
-        $langue = $row['langue'];
+    $row = $result->fetch_assoc();
+    $nom = $row['nom'];
+    $population = $row['Population'];
+    $langue = $row['langue'];
 
     if (!$row) {
         header("location: /africa-geo-junior/adminPage.php");
         exit;
     }
+} 
 
-    } else {
-// POST method: to post and update the data of the country;
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $nom = $_POST["nom"];
-    $population = $_POST["population"];
-    $langue = $_POST["langue"];
-    $continent = $_POST["continent"];
+
+    // POST method: to post and update the data of the country;
+    // if (!isset($_POST["id_pays"]) || empty($_POST["id_pays"])) {
+        // header("location: /africa-geo-junior/views/editCountry.php");
+        // exit;
+        // echo "5";
+    // }
+    // $id = intval($_POST["id_pays"]);
+    // $nom = $_POST["nom"];
+    // $population = $_POST["Population"];
+    // $langue = $_POST["langue"];
+    // $continent = $_POST["continent_id"];
+
+
+    // do {
+    //     echo "1";
+    //     if (empty($nom) || empty($population) || empty($langue) || empty($continent)) {
+    //         $errorMessage = "All the fields are required";
+    //         break;
+    //     }
+
+    //     // insert new country to the db
+    //     $sql = "UPDATE pays 
+    //                 SET `nom`= '$nom', 
+    //                     `Population`= '$population', 
+    //                     `langue` = '$langue', 
+    //                     `continent_id` = '$continent'
+    //             WHERE id_pays = $id";
+    //     $result = $connct->query($sql);
+
+    //     if (!$result) {
+    //         $errorMessage = "Invalid query " . $connect->error;
+    //         break;
+    //         echo "2";
+    //     }
+
+
+    //     $succesMessage = "Country added correctly";
+
+    //     // header("location: /africa-geo-junior/adminPage.php");
+    //     // exit;
+    // } while (false);
+
+    $id = $_POST['id_pays'];
+    $nom = $_POST['nom'];
+    $langue = $_POST['langue'];
+    $population = $_POST['Population'];
+    $continent = $_POST['continent_id'];
 
     do {
-            if (empty($nom) || empty($population) || empty($langue)) {
-                $errorMessage = "All the fields are required";
-                break;
-            }
+        if (empty($nom) || empty($population) || empty($langue) || empty($continent)) {
+                    $errorMessage = "All the fields are required";
+                    break;
+        }
 
-            // insert new country to the db
-            $sql = "INSERT INTO pays (`nom`, `population`, `langue`, `continent_id`) VALUES ('$nom', '$population', '$langue', '$continent')";
-            $result = $connect -> query($sql);
+        $sql = "UPDATE pays " .
+        "SET `nom`= '$nom', `Population`= '$population', `langue` = '$langue', `continent_id` = '$continent'" .
+        "WHERE id_pays = $id";
+        $result = $connect->query($sql);
 
-            if (!$result) {
-                $errorMessage = "Invalid query " . $connect->error;
-                break;
-            }
+        if (!$result) {
+            $errorMessage = "Invalid query: " . $connect->error;
+            break;
+        }
 
-            $nom = "";
-            $population = "";
-            $langue = "";
-
-            $succesMessage = "Country added correctly";
-
-            header("location: /africa-geo-junior/adminPage.php");
-            exit;
-        } while (false);
-    }
+        $succesMessage = "Pays modifier avec success";
+        header("location: /africa-geo-junior/adminPage.php");
+        exit;
+    } while (true);
 }
+
 
 ?>
 
@@ -123,35 +119,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body>
-        <header class="bg-gray-300 h-16 flex justify-center items-center">
-            <nav class="h-12 w-[90%] flex justify-between items-center">
-                <div class="hover:scale-105 hover:transition-all duration-300 ease-in-out">
-                    <a href="../index.html">
-                        <img src="../assets/africa.png" alt="" class="h-12">
-                    </a>
+    <header class="bg-gray-300 h-16 flex justify-center items-center">
+        <nav class="h-12 w-[90%] flex justify-between items-center">
+            <div class="hover:scale-105 hover:transition-all duration-300 ease-in-out">
+                <a href="../index.html">
+                    <img src="../assets/africa.png" alt="" class="h-12">
+                </a>
+            </div>
+
+            <div class="flex items-center gap-20">
+                <div class="flex w-fit gap-6 justify-between">
+                    <ul class="flex gap-6 justify-between">
+                        <li class="btn bg-[#f2902f] w-24 p-2 rounded-lg flex items-center justify-center font-semibold hover:bg-[#c66505] hover:transition-all duration-300 ease-in-out">
+                            <a href="../adminPage.php">AUTHORS</a>
+                        </li>
+                        <li class="btn bg-[#f2902f] w-24 p-2 rounded-lg flex items-center justify-center font-semibold hover:bg-[#c66505] hover:transition-all duration-300 ease-in-out">
+                            <a href="">STUDENT</a>
+                        </li>
+                    </ul>
                 </div>
-    
-                <div class="flex items-center gap-20">
-                    <div class="flex w-fit gap-6 justify-between">
-                        <ul class="flex gap-6 justify-between">
-                            <li class="btn bg-[#f2902f] w-24 p-2 rounded-lg flex items-center justify-center font-semibold hover:bg-[#c66505] hover:transition-all duration-300 ease-in-out">
-                                <a href="../adminPage.php">AUTHORS</a>
-                            </li>
-                            <li class="btn bg-[#f2902f] w-24 p-2 rounded-lg flex items-center justify-center font-semibold hover:bg-[#c66505] hover:transition-all duration-300 ease-in-out">
-                                <a href="">STUDENT</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </header>
+            </div>
+        </nav>
+    </header>
 
 
     <div id="countryModal" class="fixed absolute top-[17.5%] left-[20%]">
         <div class="bg-gray-400 rounded-lg shadow-lg w-[45rem] p-6">
             <h2 class="text-xl font-bold mb-4">Modifier un Pays</h2>
             <form method="post">
-                <input type="hidden" value="<?php echo "$id" ?>">
+                <input type="hidden" name="id_pays" value="<?php echo "$id" ?>">
                 <?php
                 if (!empty($errorMessage)) {
                     echo "
@@ -166,28 +162,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <div class="mb-4">
                     <label for="nom" class="block text-black font-medium mb-2">Nom du Pays:</label>
                     <input type="text" id="nom" name="nom" class="w-full border border-gray-300 px-4 py-2 rounded-lg" value="<?php echo "$nom" ?>">
+                    
                 </div>
 
                 <div class="mb-4">
                     <label for="population" class="block text-black font-medium mb-2">Population:</label>
-                    <input type="number" id="population" name="population" class="w-full border border-gray-300 px-4 py-2 rounded-lg" value="<?php echo "$population" ?>">
+                    <input type="number" id="population" name="Population" class="w-full border border-gray-300 px-4 py-2 rounded-lg" value="<?php echo "$population" ?>">
                 </div>
-                
+
                 <div class="mb-4">
                     <label for="continent" class="block text-black font-medium mb-2">Continent:</label>
-                    <select id="continent" name="continent" class="w-full border border-gray-300 px-4 py-2 rounded-lg">
-                        <option value="" disabled selected>Sélectionnez un Continent</option>
-                        
-                        <?php 
-                            $result = $connect->query("SELECT continent_id, nom FROM continent");
-                            while ($row = $result->fetch_assoc()) {
-                                echo "<option value='" . $row['continent_id'] . "'>" . $row['nom'] . "</option>";
-                            } 
+                    <select id="continent" name="continent_id" class="w-full border border-gray-300 px-4 py-2 rounded-lg">
+                        <option value="" disabled selected>Selectioner une continent..</option>
+
+                        <?php
+                        $result = $connect->query("SELECT continent_id, nom FROM continent");
+                        while ($row = $result->fetch_assoc()) {
+                            echo "<option value='" . $row['continent_id'] . "'>" . $row['nom'] . "</option>";
+                        }
                         ?>
 
                     </select>
                 </div>
-                
+
                 <div class="mb-4">
                     <label for="langue" class="block text-black font-medium mb-2">Langue:</label>
                     <input type="text" id="langue" name="langue" class="w-full border border-gray-300 px-4 py-2 rounded-lg" value="<?php echo "$langue" ?>">
